e142a983ec624308a33b6677641b8983f8665257
Shushmita <Shushmitat39@gmail.com>
2025-12-29 14:32:25 +0530

    created testimonials section

--- landing-page/src/App.js
+++ landing-page/src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Home from './Components/Home';
 import About from './Components/About'
+import Testimonials from './Components/Testimonials';
 
 function App() {
   return <div>
     <Home></Home>
     <About></About>
+    <Testimonials></Testimonials>
   </div>
 }
 export default App;
